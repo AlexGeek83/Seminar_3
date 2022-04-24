e27dcd79f36d8d8a58573ad08040f32f28b5a10e
Alex <lrl.msk@yandex.ru>
2022-04-24 21:24:11 +0300

    add Zadacha_1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая принимает на вход координаты точкт (X Y), причем X не равен 0 ,Y не равен 0
+// и выдает номер четверти плоскости в которой находится эта точка.
+
+int x, y;
+x = -2;
+y = 2;
+
+if (x > 0)
+{
+    if (y > 0)
+    {
+        System.Console.WriteLine("1-я четверть");
+    }
+    else 
+        System.Console.WriteLine("4-я четверть");
+}
+else 
+    if (y > 0)
+    {
+        System.Console.WriteLine("2-я четверть");
+    }
+    else 
+        System.Console.WriteLine("3-я четверть");
