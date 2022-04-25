b57017f16adcba808f233e74af9a172f66436224
Alex <lrl.msk@yandex.ru>
2022-04-25 08:36:52 +0300

    add Zadacha_2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// программа, которая по заданному номеру четверти, показывает диапазон
+// возможных точек координат в этой четверти (x и y)
+
+//int plane1 = x > 0 to inf;
+//int plane1 = x < 0 to -inf;
+//int plane1 = y > 0 to inf;
+//int plane1 = y < 0 to -inf;
+
+Console.Clear();
+
+System.Console.WriteLine("Введите № четверти");
+
+string plane = Console.ReadLine();
+int a = Convert.ToInt32(plane);
+if(a == 1)
+{
+    System.Console.WriteLine("x положительный и y положительный");
+}
+if(a == 2)
+{
+    System.Console.WriteLine("x отрицательное и y положительный");
+}
+if(a == 3)
+{
+    System.Console.WriteLine("x отрицательное и y отрицательное");
+}
+if(a == 4)
+{
+    System.Console.WriteLine("x положительный и y отрицательное");
+}
