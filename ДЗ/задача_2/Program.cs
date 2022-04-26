e7438169b37d1867f3e8cf4531c7f1d7a98f4608
Alex <lrl.msk@yandex.ru>
2022-04-26 08:52:17 +0300

    add дз -  Zadacha_2

---
+++ ДЗ/задача_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход координаты 2-х точек и находит
+// расстояние между ними в 3D пространстве
+// А(3,6,8); В(2,1,-7), -> 15.84
+// А(7,-5,0); В(1,-1,9), -> 11.53
+
+Console.WriteLine("Hello");
+
+double xa = 7;
+double ya = -5;
+double za = 0;
+double xb = 1;
+double yb = -1;
+double zb = 9;
+
+double S = ((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb) + (za - zb) * (za - zb));  //теорема пифагора
+S = Math.Sqrt(S);
+System.Console.Write("Расстояние между точками А и В равно = ");
+System.Console.WriteLine(S);
+
+
